2f86d40f4fe97f39d501202ae2f53d2b6aab5936
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-29 18:40:56 +0300

    add new exercise

---
+++ src/WorkingWithAbstraction/Lab/HotelReservation/Season.java
@@ -0,0 +1,21 @@
+package WorkingWithAbstraction.Lab.HotelReservation;
+
+public enum Season {
+    AUTUMN(1),
+    SPRING(2),
+    WINTER(3),
+    SUMMER(4);
+    private final int multiplier;
+
+    Season(int multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    public int getMultiplier() {
+        return multiplier;
+    }
+
+    public static Season parse(String str) {
+        return Season.valueOf(str.toUpperCase());
+    }
+}
