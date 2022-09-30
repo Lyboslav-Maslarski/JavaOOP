7be5d804789d5113503f579472097c30496aed54
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-09-30 21:44:48 +0300

    add new exercise

--- src/WorkingWithAbstraction/Exercise/greedyTimes/Main.java
+++ src/WorkingWithAbstraction/Exercise/greedyTimes/Main.java
@@ -1,98 +1,24 @@
-
 package WorkingWithAbstraction.Exercise.greedyTimes;
 
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        long vhod = Long.parseLong(scanner.nextLine());
-        String[] seif = scanner.nextLine().split("\\s+");
 
-        var torba = new LinkedHashMap<String, LinkedHashMap<String, Long>>();
-        long zlato = 0;
-        long kamuni = 0;
-        long mangizi = 0;
+        long capacity = Long.parseLong(scanner.nextLine());
+        String[] loot = scanner.nextLine().split("\\s+");
 
-        for (int i = 0; i < seif.length; i += 2) {
-            String name = seif[i];
-            long broika = Long.parseLong(seif[i + 1]);
+        Bag bag = new Bag(capacity);
 
-            String kvoE = "";
+        for (int i = 0; i < loot.length; i += 2) {
 
-            if (name.length() == 3) {
-                kvoE = "Cash";
-            } else if (name.toLowerCase().endsWith("gem")) {
-                kvoE = "Gem";
-            } else if (name.toLowerCase().equals("gold")) {
-                kvoE = "Gold";
-            }
+            String currentLoot = loot[i];
+            long amount = Long.parseLong(loot[i + 1]);
 
-            if (kvoE.equals("")) {
-                continue;
-            } else if (vhod < torba.values().stream().map(Map::values).flatMap(Collection::stream).mapToLong(e -> e).sum() + broika) {
-                continue;
-            }
-
-            switch (kvoE) {
-                case "Gem":
-                    if (!torba.containsKey(kvoE)) {
-                        if (torba.containsKey("Gold")) {
-                            if (broika > torba.get("Gold").values().stream().mapToLong(e -> e).sum()) {
-                                continue;
-                            }
-                        } else {
-                            continue;
-                        }
-                    } else if (torba.get(kvoE).values().stream().mapToLong(e -> e).sum() + broika > torba.get("Gold").values().stream().mapToLong(e -> e).sum()) {
-                        continue;
-                    }
-                    break;
-                case "Cash":
-                    if (!torba.containsKey(kvoE)) {
-                        if (torba.containsKey("Gem")) {
-                            if (broika > torba.get("Gold").values().stream().mapToLong(e -> e).sum()) {
-                                continue;
-                            }
-                        } else {
-                            continue;
-                        }
-                    } else if (torba.get(kvoE).values().stream().mapToLong(e -> e).sum() + broika > torba.get("Gem").values().stream().mapToLong(e -> e).sum()) {
-                        continue;
-                    }
-                    break;
-            }
-
-            if (!torba.containsKey(kvoE)) {
-                torba.put((kvoE), new LinkedHashMap<String, Long>());
-            }
-
-            if (!torba.get(kvoE).containsKey(name)) {
-                torba.get(kvoE).put(name, 0L);
-            }
-
-
-            torba.get(kvoE).put(name, torba.get(kvoE).get(name) + broika);
-            if (kvoE.equals("Gold")) {
-                zlato += broika;
-            } else if (kvoE.equals("Gem")) {
-                kamuni += broika;
-            } else if (kvoE.equals("Cash")) {
-                mangizi += broika;
-            }
-        }
-
-        for (var x : torba.entrySet()) {
-            Long sumValues = x.getValue().values().stream().mapToLong(l -> l).sum();
-
-            System.out.println(String.format("<%s> $%s", x.getKey(), sumValues));
-
-            x.getValue().entrySet().stream().sorted((e1, e2) -> e2.getKey().compareTo(e1.getKey())).forEach(i -> System.out.println("##" + i.getKey() + " - " + i.getValue()));
+            bag.lootMaterial(currentLoot, amount);
 
         }
+        System.out.println(bag);
     }
 }
