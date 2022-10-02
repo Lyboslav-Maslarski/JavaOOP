b8bfb24c057843a70d95e186c3ba1cc9db9dd19f
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-10-02 18:49:55 +0300

    add new exercise

---
+++ src/Encapsulation/Exercise/ClassBox/Main.java
@@ -0,0 +1,16 @@
+package Encapsulation.Exercise.ClassBox;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double length = Double.parseDouble(scan.nextLine());
+        double width = Double.parseDouble(scan.nextLine());
+        double height = Double.parseDouble(scan.nextLine());
+        Box box = new Box(length, width, height);
+        System.out.println("Surface Area - " + box.calculateSurfaceArea());
+        System.out.println("Lateral Surface Area - " + box.calculateLateralSurfaceArea());
+        System.out.println("Volume - " + box.calculateVolume());
+    }
+}
