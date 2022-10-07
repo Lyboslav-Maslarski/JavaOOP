d6375592e8ad5807b27ec2497f4a59272def1103
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-10-07 12:03:08 +0300

    add new exercise

---
+++ src/InterfacesAndAbstraction/Lab/SayHello/European.java
@@ -0,0 +1,12 @@
+package InterfacesAndAbstraction.Lab.SayHello;
+
+public class European extends BasePerson {
+    public European(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Hello";
+    }
+}
