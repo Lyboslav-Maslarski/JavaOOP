21b5dff5f6e9ffdc14e6cae267e251bde96926eb
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2022-10-13 17:39:07 +0300

    add new exercise

---
+++ src/Polymorphism/Exercise/WildFarm/Mouse.java
@@ -0,0 +1,22 @@
+package Polymorphism.Exercise.WildFarm;
+
+public class Mouse extends Mammal {
+
+    public Mouse(String animalType, String animalName, Double animalWeight, String livingRegion) {
+        super(animalType, animalName, animalWeight, livingRegion);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("SQUEEEAAAK!");
+    }
+
+    @Override
+    public void eat(Food food) {
+        if (food instanceof Vegetable) {
+            setFoodEaten(getFoodEaten() + food.getQuantity());
+        } else {
+            System.out.println("Mice are not eating that type of food!");
+        }
+    }
+}
