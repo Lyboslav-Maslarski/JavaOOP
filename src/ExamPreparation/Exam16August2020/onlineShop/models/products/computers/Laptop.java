3dcbc56eaaab45d8289e1b51d037aeb903f40d1e
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-03-20 22:51:19 +0200

    add new exercise

---
+++ src/ExamPreparation/Exam16August2020/onlineShop/models/products/computers/Laptop.java
@@ -0,0 +1,9 @@
+package ExamPreparation.Exam16August2020.onlineShop.models.products.computers;
+
+public class Laptop extends BaseComputer{
+    private final static double OVERALL_PERFORMANCE = 10;
+
+    public Laptop(int id, String manufacturer, String model, double price) {
+        super(id, manufacturer, model, price, OVERALL_PERFORMANCE);
+    }
+}
