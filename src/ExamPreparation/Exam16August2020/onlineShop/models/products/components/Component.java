ceceb5fa7c0e57dbad987ebec3d7f41473483205
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-03-20 22:49:51 +0200

    add new exercise

---
+++ src/ExamPreparation/Exam16August2020/onlineShop/models/products/components/Component.java
@@ -0,0 +1,8 @@
+package ExamPreparation.Exam16August2020.onlineShop.models.products.components;
+
+import ExamPreparation.Exam16August2020.onlineShop.models.products.Product;
+
+public interface Component extends Product {
+
+    int getGeneration();
+}
