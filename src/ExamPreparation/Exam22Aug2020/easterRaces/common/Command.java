2935cc7eadb2e6b5653e35097863b3bd4ff055ce
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-03-21 20:08:33 +0200

    add new exercise

---
+++ src/ExamPreparation/Exam22Aug2020/easterRaces/common/Command.java
@@ -0,0 +1,11 @@
+package ExamPreparation.Exam22Aug2020.easterRaces.common;
+
+public enum Command {
+    CreateDriver,
+    CreateCar,
+    AddCarToDriver,
+    AddDriverToRace,
+    CreateRace,
+    StartRace,
+    End
+}
