89d2f05903a4e60d8512d38cc4adc87c7cb4a9f4
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-03-24 21:14:27 +0200

    add new exercise

---
+++ src/ExamPreparation/Exam12Dec2020/bakery/entities/bakedFoods/interfaces/BakedFood.java
@@ -0,0 +1,9 @@
+package ExamPreparation.Exam12Dec2020.bakery.entities.bakedFoods.interfaces;
+
+public interface BakedFood {
+    String getName();
+
+    double getPortion();
+
+    double getPrice();
+}
