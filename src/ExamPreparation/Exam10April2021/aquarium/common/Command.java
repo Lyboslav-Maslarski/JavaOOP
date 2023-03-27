c09d22311fcf179d60866da980537eea09cc2c0a
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-03-28 00:55:48 +0300

    add new exercise

---
+++ src/ExamPreparation/Exam10April2021/aquarium/common/Command.java
@@ -0,0 +1,12 @@
+package ExamPreparation.Exam10April2021.aquarium.common;
+
+public enum Command {
+    AddAquarium,
+    AddDecoration,
+    InsertDecoration,
+    AddFish,
+    FeedFish,
+    CalculateValue,
+    Report,
+    Exit
+}
