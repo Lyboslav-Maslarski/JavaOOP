0ef0f10fd29e76693db441ac9856db9b0d12fce5
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-03-28 00:58:06 +0300

    add new exercise

---
+++ src/ExamPreparation/Exam10April2021/aquarium/entities/aquariums/FreshwaterAquarium.java
@@ -0,0 +1,10 @@
+package ExamPreparation.Exam10April2021.aquarium.entities.aquariums;
+
+public class FreshwaterAquarium extends BaseAquarium {
+
+    public static final int CAPACITY = 50;
+
+    public FreshwaterAquarium(String name) {
+        super(name, CAPACITY);
+    }
+}
---
+++ src/ExamPreparation/Exam18April2021/spaceStation/models/astronauts/Geodesist.java
@@ -0,0 +1,10 @@
+package ExamPreparation.Exam18April2021.spaceStation.models.astronauts;
+
+public class Geodesist extends BaseAstronaut{
+
+    public static final int OXYGEN = 50;
+
+    public Geodesist(String name) {
+        super(name, OXYGEN);
+    }
+}
