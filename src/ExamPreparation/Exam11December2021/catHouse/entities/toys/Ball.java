5d6f86d4b11716dde769e288bebcaf856fb2a0c5
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-03-31 20:00:55 +0300

    add new exercise

---
+++ src/ExamPreparation/Exam11December2021/catHouse/entities/toys/Ball.java
@@ -0,0 +1,11 @@
+package ExamPreparation.Exam11December2021.catHouse.entities.toys;
+
+public class Ball extends BaseToy {
+
+    public static final int SOFTNESS = 1;
+    public static final int PRICE = 10;
+
+    public Ball() {
+        super(SOFTNESS, PRICE);
+    }
+}
