cf941c2c939352240e4544c5ee3e4e2486053b52
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-04-07 23:10:44 +0300

    add new exercise

---
+++ src/ExamPreparation/Exam09April2022/fairyShop/common/Command.java
@@ -0,0 +1,10 @@
+package ExamPreparation.Exam09April2022.fairyShop.common;
+
+public enum Command {
+    AddHelper,
+    AddPresent,
+    AddInstrumentToHelper,
+    CraftPresent,
+    Report,
+    Exit,
+}
---
+++ src/ExamPreparation/Exam19December2022/magicGame/common/Command.java
@@ -0,0 +1,9 @@
+package ExamPreparation.Exam19December2022.magicGame.common;
+
+public enum Command {
+    AddMagic,
+    AddMagician,
+    Report,
+    StartGame,
+    Exit
+}
