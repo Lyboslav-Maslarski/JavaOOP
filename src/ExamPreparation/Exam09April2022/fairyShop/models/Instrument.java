f0073b58c7e112ef17aacb59bf4c4f03942d3257
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-04-07 23:12:11 +0300

    add new exercise

---
+++ src/ExamPreparation/Exam09April2022/fairyShop/models/Instrument.java
@@ -0,0 +1,9 @@
+package ExamPreparation.Exam09April2022.fairyShop.models;
+
+public interface Instrument {
+    int getPower();
+
+    void use();
+
+    boolean isBroken();
+}
---
+++ src/ExamPreparation/Exam19December2022/magicGame/models/magics/Magic.java
@@ -0,0 +1,9 @@
+package ExamPreparation.Exam19December2022.magicGame.models.magics;
+
+public interface Magic {
+    String getName();
+
+    int getBulletsCount();
+
+    int fire();
+}
